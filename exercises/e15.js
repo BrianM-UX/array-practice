dfc99a2eacef749e9983c595c81191feedbcad36
brian_mc <bfocus44@gmail.com>
2024-02-02 00:09:29 -0600

    completed e15

--- exercises/e15.js
+++ exercises/e15.js
@@ -6,6 +6,7 @@ import { data } from "../data/data";
 
 export function getPlanetsWithNoMoons(data) {
   // Your code goes here...
+  return data.planets.filter(noMoons => !noMoons.moons).map(planet => planet.name)
 }
 
 
